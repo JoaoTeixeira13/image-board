8403ff289dd948144746a211b2cb98e214612224
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-07-02 14:23:51 +0200

    modal box layout and general application css styling

--- public/js/modal.js
+++ public/js/modal.js
@@ -10,39 +10,30 @@ const modal = {
     props: ["selectedImage"],
     mounted() {
         console.log("first component mounted");
-        console.log("selected image mounted  is ", this.selectedImage);
-        // console.log("this is ", this);
-        //fetch request
+
         fetch(`/getImages/${this.selectedImage}`)
             .then((res) => res.json())
             .then((data) => {
                 this.image = data;
-                console.log("current data is", data);
-                console.log(this.image, "this image object");
             })
             .catch((err) => {
                 console.log("error is ", err);
             });
     },
     methods: {
-        increaseCount() {
-            console.log("user wants to count");
-            this.count++;
+        close() {
+            this.$emit("close");
         },
-        close(){
-            console.log("this is being clicked");
-            this.$emit("close")
-            
-        }
     },
-    template: ` <div>
-    <span @click="close">X</span>
-                    <img v-bind:src="image.url" v-bind:alt="image.description"/>
-                    <h2>{{image.title}}</h2>
-                    <h3>{{image.description}}</h3>
-                    <p>Uploaded by {{image.username}} on {{image.created_at}}</p>
-                     <button @click="increaseCount"> increase count </button>
-                     <button @click="count--">decrease count </button>
+    template: ` <div class="modal-box">
+                    <img v-bind:src="image.url" v-bind:alt="image.description" :key="image.id"/>
+                    <div class="modal-info">
+                        <h2>{{image.title}}</h2>
+                        <h3>{{image.description}}</h3>
+                        <p>Uploaded by {{image.username}} on {{image.created_at}}</p>
+                    </div>
+                    <span @click="close" class=close-tag>X</span>
+                    
                </div>`,
 };
 
