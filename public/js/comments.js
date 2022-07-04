55086c616c52a34605fafa71bc349ac043bc4fd8
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-07-05 00:53:53 +0200

    comment section styling

--- public/js/comments.js
+++ public/js/comments.js
@@ -44,21 +44,26 @@ const comments = {
     },
     template: ` <div class="comment-template">
 
-                    <h3>Add a Comment!</h3>
-
-                    <div class="comment-form">
-                        <input v-model="comment" name="comment" placeholder="COMMENT*" required/>
-                        <input v-model="username" name="username" placeholder="USERNAME*" required/>
-                        <button @click="submit"><i class="fa-solid fa-circle-arrow-up"></i> Submit</button> 
-
-                    </div>
-
-                    <div v-if="comments.length" v-for="comment in comments" :key="comment.id" class="comment-box">
-                    
-                        <p>{{comment.comment}}</p>
-                        <p>{{comment.username}} on {{comment.created_at}}.</p>
+                    <div class="comment-group">
+    
+                        <div v-if="comments.length" v-for="comment in comments" :key="comment.id" class="comment-box">
                         
+                            <p>{{comment.comment}}</p>
+                            <p><strong>{{comment.username}}</strong> on {{comment.created_at}}.</p>
+                        
+                        </div>
                     </div>
+                    
+                        <div class="comment-form">
+                    
+                            <div class="comment-inputs">
+                                <input v-model="comment" name="comment" placeholder="COMMENT*" required/>
+                                <input v-model="username" name="username" placeholder="USERNAME*" required/>
+                            </div>
+                            
+                            <button @click="submit"><i class="fa-solid fa-circle-arrow-up"></i> Submit</button> 
+
+                        </div>
 
                 </div>
                 `,
